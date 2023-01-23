b9df7df834999be1da8d598a3163a7805a2de713
keepreverse <supreme2309@mail.ru>
2023-01-23 18:06:42 +0300

    small fixes

--- src/obj/building/chest_treasure.py
+++ src/obj/building/chest_treasure.py
@@ -5,6 +5,7 @@ from src.obj.building.trigger_building import TriggerBuilding
 from src.obj.entity.item.treasure import Treasure
 from src.obj.entity.item.key import Key
 from src.obj.entity.item.potion_hp import PotionHP
+from src.obj.entity.item.potion_buff import PotionBuff
 from src.tool.vector import Vector
 """
 Chest with a treasure
@@ -14,7 +15,7 @@ Chest with a treasure
 class ChestTreasure(TriggerBuilding):
     def __init__(self, pos, *collectibles):
         super().__init__(pos, IMAGE['chest'][0], (0, 0), False, False, IMAGE['chest'][1], (0, 0))
-        self.__collectibles = [*collectibles, Treasure(), Key(), PotionHP()]
+        self.__collectibles = [*collectibles, Treasure(), Key(), PotionHP(), PotionBuff()]
 
     @property
     def collectibles(self):
